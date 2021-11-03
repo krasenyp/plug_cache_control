5e2076be923e839408a12fe53397e8193ffc8885
Krasen Penchev <krasen.penchev@protonmail.com>
2021-11-03 11:44:26 +0200

    Add package information

--- mix.exs
+++ mix.exs
@@ -1,6 +1,7 @@
 defmodule Plug.CacheControl.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/krasenyp/plug_cache_control"
   @version "0.1.0"
 
   def project do
@@ -9,7 +10,15 @@ defmodule Plug.CacheControl.MixProject do
       version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      description: "A convenience for manipulating cache-control header values.",
+      package: package(),
+
+      # Docs
+      name: "Plug.CacheControl",
+      docs: docs()
     ]
   end
 
@@ -26,6 +35,22 @@ defmodule Plug.CacheControl.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:ex_doc, "~> 0.25", only: :dev, runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Krasen Penchev"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url},
+      files: ~w(.formatter.exs mix.exs README.md lib)
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Plug.CacheControl",
+      source_ref: "v#{@version}"
     ]
   end
 end
