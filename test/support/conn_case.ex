dd3e8021b9b5cb9e9505dfce4a6c9ded4ba475dd
Krasen Penchev <krasen.penchev@protonmail.com>
2021-11-05 22:23:37 +0200

    Fix credo warnings

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -1,4 +1,6 @@
 defmodule Plug.CacheControl.ConnCase do
+  @moduledoc false
+
   use ExUnit.CaseTemplate
 
   alias Plug.Conn
@@ -10,7 +12,7 @@ defmodule Plug.CacheControl.ConnCase do
     end
   end
 
-  setup tags do
+  setup _tags do
     {:ok, conn: build_conn()}
   end
 
@@ -20,22 +22,10 @@ defmodule Plug.CacheControl.ConnCase do
     |> List.first()
   end
 
-  @doc """
-  Creates a connection to be used in upcoming requests.
-  """
-  @spec build_conn() :: Conn.t()
-  defp build_conn() do
-    build_conn(:get, "/", nil)
+  defp build_conn do
+    build_conn(:get, "/")
   end
 
-  @doc """
-  Creates a connection to be used in upcoming requests
-  with a preset method, path and body.
-
-  This is useful when a specific connection is required
-  for testing a plug or a particular function.
-  """
-  @spec build_conn(atom | binary, binary, binary | list | map | nil) :: Conn.t()
   defp build_conn(method, path, params_or_body \\ nil) do
     Plug.Adapters.Test.Conn.conn(%Conn{}, method, path, params_or_body)
     |> Conn.put_private(:plug_skip_csrf_protection, true)
